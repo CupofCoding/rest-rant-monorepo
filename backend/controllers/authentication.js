822c1ab247fb0d7f0183a47cd6989831305262cc
Cup of Coding <anne@cupofcoding.com>
2022-06-27 12:41:40 -0700

    step two assignment 4

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -8,6 +8,36 @@ const { User } = db
 //     console.log('IN HERE')
 // })
   
+// router.post('/', async (req, res) => {
+    
+//     let user = await User.findOne({
+//         where: { email: req.body.email }
+//     })
+
+//     if (!user || !await bcrypt.compare(req.body.password, user.passwordDigest)) {
+//         res.status(404).json({ 
+//             message: `Could not find a user with the provided username and password` 
+//         })
+//     } else {
+//         res.json({ user })
+//     }
+// })
+
+// // ___
+// router.get('/profile', async (req, res) => {
+//     try {
+//         let user = await User.findOne({
+//             where: {
+//                 userId:  
+//             }
+//         })
+//         res.json(user)
+//     } catch {
+//         res.json(null)
+//     }
+// })
+
+___  
 router.post('/', async (req, res) => {
     
     let user = await User.findOne({
@@ -19,8 +49,22 @@ router.post('/', async (req, res) => {
             message: `Could not find a user with the provided username and password` 
         })
     } else {
-        res.json({ user })
+        res.json({ user })                                       
     }
 })
+
+router.get('/profile', async (req, res) => {
+    try {
+        let user = await User.findOne({
+            where: {
+                userId:                      
+            }
+        })
+        res.json(user)
+    } catch {
+        res.json(null)
+    }
+})
+
 
 module.exports = router
